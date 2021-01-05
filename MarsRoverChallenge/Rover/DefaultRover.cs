e8b63bf9b21dc3114e383a739020f9e020d2d4d3
Aleks <kriegeraleks@protonmail.com>
2021-01-05 22:53:02 +0200

    Add project files.

---
+++ MarsRoverChallenge/Rover/DefaultRover.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsRoverChallenge.Rover
+{
+    class DefaultRover
+    {
+    }
+}
---
+++ MarsRoverChallenge/Rover/IRover.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsRoverChallenge.Rover
+{
+    enum TurnDirection
+    {
+        Left ,
+        Right
+    }
+
+
+    interface IRover
+    {
+         
+
+    }
+}
