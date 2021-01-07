7a77bfe7e89a15d745d6f8d2fd37360a2e0f2256
Aleks <kriegeraleks@protonmail.com>
2021-01-07 20:21:00 +0200

    Filling in the BaseRover abstract class
    This includes the thinking for why exactly the class is structured this
    way as well as exactly what the methods are meant to return to the
    caller given the context

--- MarsRoverChallenge/Rover/BaseRover.cs
+++ MarsRoverChallenge/Rover/BaseRover.cs
@@ -1,4 +1,4 @@
-
+﻿
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,6 +12,20 @@ namespace MarsRoverChallenge.Rover
          *  This is meant to represent the base classes for any future rover implementations
          */
     {
+
+        /**
+         *  Move is to be used to return the future position in space that the rover intends to occupy
+         *  if it obeys the given command. This is here because different kinds of rovers could potentially 
+         *  move in different ways
+        */
+        public abstract Position move(Position currentPosition, RoverInstruction command );
+
+        /**
+         * Because in reality a rover is responsible for ensuring it does not fall off a cliff this is 
+         * the logical place to put the logic for the check. After all, not all rovers have wheels
+         */
+        public abstract bool isValidPosition(Position intendedPosition, Position boundPosition );
+            
         
 
     }
