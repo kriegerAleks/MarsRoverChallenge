1eaa0985879768fd3acd2483e90f6a91bd45b625
Aleks <kriegeraleks@protonmail.com>
2021-01-07 20:19:35 +0200

    Creating the position subclass, including X, Y and facing along with tostring and Deconstruct

--- MarsRoverChallenge/Rover/Position.cs
+++ MarsRoverChallenge/Rover/Position.cs
@@ -1,3 +1,4 @@
+﻿using MarsRoverChallenge.Rover;
 
 namespace MarsRoverChallenge
 {
@@ -6,6 +7,25 @@ namespace MarsRoverChallenge
      * I position denotes a current position on the grid. It represents a stationary position
      */
     {
+        public Position(int x, int y, Direction facing = Direction.North )
+        {
+            X = x; 
+            Y = y; 
+            this.facing = facing;
+        }
+        public int X { get; }
 
+        public int Y { get; }
+
+        // can be nulled if not needed
+        public Direction facing { get; set; }
+
+        public override string ToString() => $"{X} {Y} {facing}";
+
+        public void Deconstruct(out int X, out int Y, out Direction facing) {
+            X = this.X;
+            Y = this.Y;
+            facing = this.facing;
+        }
     }
 }
