f7ca5f519593dfe3e446fb36fe667e6f7342d3d4
Aleks <kriegeraleks@protonmail.com>
2021-01-07 20:23:30 +0200

    Creating the default rover class
    Overrides the RoverBase methods namely
    	isValidPosition
    	move
    Includes calls into the submethods for turning and moving directions.

--- MarsRoverChallenge/Rover/DefaultRover.cs
+++ MarsRoverChallenge/Rover/DefaultRover.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +6,76 @@ namespace MarsRoverChallenge.Rover
 {
     class DefaultRover : BaseRover
     {
+
+
+        /*
+         This returns the position the rover would be moving to
+         */
+        public override Position move(Position currentPosition, RoverInstruction command)
+        {
+
+            switch (command)
+            {
+                case RoverInstruction.Left:
+                case RoverInstruction.Right:
+                    return turn(currentPosition, command);
+                default:
+                    return moveForward(currentPosition);
+
+            }
+
+
+        }
+
+        public override bool isValidPosition(Position intendedPosition, Position boundPosition)
+        {
+            bool xOutsideBounds = intendedPosition.X > boundPosition.X || intendedPosition.X < 0;
+            bool yOutsideBounds = intendedPosition.Y > boundPosition.Y || intendedPosition.Y < 0;
+
+            return xOutsideBounds || yOutsideBounds;
+        }
+
+        private Position turn(Position currentPosition, RoverInstruction turnDirection)
+        {
+            int facing = 0;
+            switch (turnDirection)
+            {
+                case RoverInstruction.Left:
+                    facing = (int)currentPosition.facing - 1;
+                    break;
+                case RoverInstruction.Right:
+                    facing = (int)currentPosition.facing - 1;
+                    break;
+            }
+            Direction newFacing = (Direction)(Math.Abs(facing) % 4);
+
+            return new Position(currentPosition.X, currentPosition.Y, newFacing);
+        }
+
+        private Position moveForward(Position currentPosition)
+        {
+            var (X, Y, facing) = currentPosition;
+
+
+            switch (currentPosition.facing)
+            {
+                case Direction.North:
+                    Y += 1;
+                    break;
+                case Direction.East:
+                    X += 1;
+                    break;
+                case Direction.South:
+                    Y -= 1;
+                    break;
+                case Direction.West:
+                    X -= 1;
+                    break;
+            }
+
+            return new Position(X, Y, facing);
+        }
+
 
     }
 }
