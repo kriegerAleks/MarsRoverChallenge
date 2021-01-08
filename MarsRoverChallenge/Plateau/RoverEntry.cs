c1862c49e24e4887202258a2e0d816a98e16e7dd
Aleks <kriegeraleks@protonmail.com>
2021-01-08 10:29:37 +0200

    Created the rover entry class
    To be used to contain the list of rovers that will be used

---
+++ MarsRoverChallenge/Plateau/RoverEntry.cs
@@ -0,0 +1,28 @@
+﻿using MarsRoverChallenge.Rover;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsRoverChallenge.Plateau
+{
+     struct RoverEntry
+    {
+        public BaseRover rover { get; }
+        public Position currentPosition { get; set; }
+        public RoverInstruction[] instructions { get; }
+
+        public RoverEntry(BaseRover rover , Position currentPosition, RoverInstruction[] instructions)
+        {
+            this.rover = rover;
+            this.currentPosition = currentPosition;
+            this.instructions = instructions;
+        }
+
+        public void Deconstruct(out BaseRover rover, out Position currentPosition, out RoverInstruction[] instructions)
+        {
+            rover = this.rover;
+            currentPosition = this.currentPosition;
+            instructions = this.instructions;
+        }
+    }
+}
