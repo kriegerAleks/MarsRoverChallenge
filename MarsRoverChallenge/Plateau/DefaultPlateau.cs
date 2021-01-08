f159669b17f172ee3b00cab49ad922d5cb3f640b
Aleks <kriegeraleks@protonmail.com>
2021-01-08 10:30:14 +0200

    Create default plateau

--- MarsRoverChallenge/Plateau/DefaultPlateau.cs
+++ MarsRoverChallenge/Plateau/DefaultPlateau.cs
@@ -1,10 +1,26 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using MarsRoverChallenge.Rover;
 
 namespace MarsRoverChallenge.Plateau
 {
+    /**
+     * strictly speaking this isnt neccessary
+     */
     class DefaultPlateau
     {   
+      public Position boundary { get; }
+
+        private List<RoverEntry> roverList = new List<RoverEntry>();
+        
+       public DefaultPlateau(Position boundary)
+        {
+            this.boundary = boundary;
+        }
+
+        public void addRover(Position initalPosition, RoverInstruction[] instructions) {
+            roverList.Add(new RoverEntry(new DefaultRover(),initalPosition, instructions));
+        }
     }
 }
