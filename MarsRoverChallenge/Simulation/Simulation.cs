0d4b7b0df42e4c7dbe04260cf31e896961a99958
Aleks <kriegeraleks@protonmail.com>
2021-01-08 10:29:57 +0200

    moved simulation to its own folder

---
+++ MarsRoverChallenge/Simulation/Simulation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsRoverChallenge
+{
+    class Simulation
+    {
+        private Plateau.DefaultPlateau p;
+        public Simulation(int gridX, int gridY)
+        {
+            p = new Plateau.DefaultPlateau(gridX, gridY);
+        }
+    }
+}
