3d4f3fc22a5f893b005454ec798fa9eca165a586
Aleks <kriegeraleks@protonmail.com>
2021-01-09 11:25:18 +0200

    Updated direction with coversion functions to and from direction

--- MarsRoverChallenge/Rover/Direction.cs
+++ MarsRoverChallenge/Rover/Direction.cs
@@ -1,10 +1,46 @@
 ﻿namespace MarsRoverChallenge.Rover
 {
-    public enum Direction
+   public enum Direction
+        {
+            North,
+            East,
+            South,
+            West
+        }
+    
+   static class Directions
     {
-        North,
-        East,
-        South,
-        West
+
+        /**
+         * TODO:  There should be a better way to do this. I know that its possible
+         * to make an enum class and that using that it is theoretically possible 
+         * to override the default conversion from / to strings and numbers
+         */
+
+        public static char convertFromDirection(Direction direction)
+        {
+            switch (direction)
+            {
+
+                case Direction.North: return 'N';
+                case Direction.South: return 'S';
+                case Direction.East: return 'E';
+                default: return 'W';
+            }
+
+        }
+        public static Direction convertToDirection(char stringDirection)
+        {
+            switch (stringDirection)
+            {
+                case 'N': return Direction.North;
+                case 'S': return Direction.South;
+                case 'E': return Direction.East;
+                case 'W': return Direction.West;
+                default:
+                    throw new System.Exception("wrong symmbol");
+            }
+        }
     }
+
 }
