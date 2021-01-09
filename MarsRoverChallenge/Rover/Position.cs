454d8c065ac6cd1eed3a829f43913ecc68db7443
Aleks <kriegeraleks@protonmail.com>
2021-01-09 11:16:14 +0200

    Enhance and reorganise position struct
    BREAK: Changed the order of the constructor in position
    
    Added ToString and Deconstruct methods for ease of use

--- MarsRoverChallenge/Rover/Position.cs
+++ MarsRoverChallenge/Rover/Position.cs
@@ -1,4 +1,4 @@
-﻿using MarsRoverChallenge.Rover;
+using MarsRoverChallenge.Rover;
 
 namespace MarsRoverChallenge
 {
@@ -7,25 +7,28 @@ namespace MarsRoverChallenge
      * I position denotes a current position on the grid. It represents a stationary position
      */
     {
-        public Position(int x, int y, Direction facing = Direction.North )
+        public Position(int x, int y, Direction facing = Direction.North)
         {
-            X = x; 
-            Y = y; 
+            X = x;
+            Y = y;
             this.facing = facing;
         }
+
+        public Direction facing { get; set; }
         public int X { get; }
 
         public int Y { get; }
 
-        // can be nulled if not needed
-        public Direction facing { get; set; }
-
-        public override string ToString() => $"{X} {Y} {facing}";
-
-        public void Deconstruct(out int X, out int Y, out Direction facing) {
+        /**
+         * can be nulled if not needed
+         */
+        public void Deconstruct(out int X, out int Y, out Direction facing)
+        {
             X = this.X;
             Y = this.Y;
             facing = this.facing;
         }
+
+        public override string ToString() => $"{X} {Y} {facing}";
     }
 }
