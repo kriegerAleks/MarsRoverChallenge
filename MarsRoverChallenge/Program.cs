f4923cfdb9ca59741478d8ef1b51ae940bf45c00
Aleks <kriegeraleks@protonmail.com>
2021-01-09 12:50:52 +0200

    [feat] add private parseData fuction that returns bound, rover list
    This function will be used as the base for further tests and data entry
    as it parses a multiline string and will be able to transform that into
    the data needed to setup and run a sim

--- MarsRoverChallenge/Program.cs
+++ MarsRoverChallenge/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace MarsRoverChallenge
 {
@@ -9,4 +9,33 @@ namespace MarsRoverChallenge
             Console.WriteLine("Hello World!");
         }
     }
-}
+        private static (Position, List<(Position, List<RoverInstruction>)>) parseData(string data)
+        {
+            var lines = data.Split(Environment.NewLine);
+
+            var boundryLine = lines[0];
+
+            var boundrySpec = boundryLine.Split(' ');
+
+            var boundryPoint = new Position(int.Parse(boundrySpec[0]), int.Parse(boundrySpec[1]));
+
+            var itemList = new List<(Position, List<RoverInstruction>)>();
+
+            int offset = 1;
+            for (var currentItemIndex = offset; currentItemIndex < lines.Length; currentItemIndex += 2)
+            {
+                var roverDetails = lines[currentItemIndex].Split(' ');
+                var x = int.Parse(roverDetails[0]);
+                var y = int.Parse(roverDetails[1]);
+                var direction = Directions.convertToDirection(roverDetails[2][0]);
+
+                var position = new Position(x, y, direction);
+
+                var instructionList = lines[currentItemIndex + 1].Select(command => ConvertValueToInstruction(command)).ToList();
+
+                itemList.Add((position, instructionList));
+            }
+
+            return (boundryPoint, itemList);
+        }
+    }
