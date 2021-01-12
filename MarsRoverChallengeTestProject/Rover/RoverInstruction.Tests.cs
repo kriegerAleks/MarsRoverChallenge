2bba18f4f199b82feb5d3c51a4bae379bf944819
Aleks <kriegeraleks@protonmail.com>
2021-01-12 21:12:19 +0200

    [test]Add tests for RoverInstructions
    Add test to ensure conversion works
    Add test to ensure error is thrown

--- MarsRoverChallengeTestProject/Rover/RoverInstruction.Tests.cs
+++ MarsRoverChallengeTestProject/Rover/RoverInstruction.Tests.cs
@@ -1,10 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using MarsRoverChallenge.Rover;
+using System;
+using Xunit;
 
 namespace MarsRoverChallengeTestProject.Rover
 {
-    class RoverInstructionTests
+    public class RoverInstructionTests
     {
+        [Theory]
+        [InlineData('M',RoverInstruction.Move)]
+        [InlineData('L',RoverInstruction.Left)]
+        [InlineData('R',RoverInstruction.Right)]
+        public void ConvertValueToInstruction_ConvertsToInstruction_WhenValidValueGiven(char valueToConvert, RoverInstruction expectedInstruction)
+        {
+            var conversionResult = RoverInstructions.ConvertValueToInstruction(valueToConvert);
+
+            Assert.True(conversionResult == expectedInstruction);
+        }
+
+        [Fact]
+        public void ConvertValueToInstruction_ThrowsError_WhenInvalidValueGiven()
+        {
+            Exception testException = null;
+
+            try
+            {
+                var throwawayValue = RoverInstructions.ConvertValueToInstruction('S');
+            }
+            catch (Exception testE )
+            {
+                testException = testE;
+            }
+
+            Assert.NotNull(testException);
+        }
+
     }
 }
