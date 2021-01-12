eea64644404e9a65298c562a4cc266f3dc74db0c
Aleks <kriegeraleks@protonmail.com>
2021-01-13 00:52:44 +0200

    [test]Add test for running simulation
    Add class to contain list of rover test data
    Add class to contain simulation test data
    Add function that can be given an index and will return the data to test
    that simulation
    add function that will publically test the sim, this function is a
    theory. This method was chosen because there is a relatively large
    amount of setup that needs to be done for the simulation to be setup and
    run and as this involves heavy use of the new keyword its almost
    impossible to pass these details into the function via inline data. As
    such I have opted to house it in a separate function that will be called
    to get the simulation details. Its not the best solution but it works
    well enoughh that ill leave it untill I come up with a better way

--- MarsRoverChallengeTestProject/Simulation/Simulation.Tests.cs
+++ MarsRoverChallengeTestProject/Simulation/Simulation.Tests.cs
@@ -1,4 +1,4 @@
-using MarsRoverChallenge;
+﻿using MarsRoverChallenge;
 using MarsRoverChallenge.Rover;
 using MarsRoverChallenge.Simulation;
 using System.Collections.Generic;
@@ -48,8 +48,96 @@ namespace MarsRoverChallengeTestProject.Simulation
             Assert.Equal(instructionList, roverInstructions);
         }
 
-    }
-}
+        private class RoverData
+        {
+            public Position initialPosition;
+            public RoverInstruction[] instructions;
+            public Position ExpectedFinalPosition;
 
+            public RoverData(Position initialPosition,
+            RoverInstruction[] instructions,
+            Position ExpectedFinalPosition)
+            {
+                this.initialPosition = initialPosition;
+                this.instructions = instructions;
+                this.ExpectedFinalPosition = ExpectedFinalPosition;
+            }
+        }
+
+        private class SimulationData
+        {
+            public Position simulationBoundary;
+            public List<RoverData> roverList;
+
+            public SimulationData(Position simulationBoundary,
+            List<RoverData> roverList)
+            {
+                this.simulationBoundary = simulationBoundary;
+                this.roverList = roverList;
+            }
+        }
+
+        /**
+         * Get simulation details is the best solution to the problem of a test that is data complex
+         * To keep the test relatively light and controllable while allowing for relatively complex
+         * data interactions. In this case to run a simulation takes a fair amount of data
+         * there is no good way to pass this as in line data but having it as a massive list without
+         * using in line data is a good way to ensure that they are annoying to understand when they break
+         * this way the user will know which instruction and output set broke and so where to begin debugging
+         */
+
+        private SimulationData GetSimulationDetails(int entry)
+        {
+            var roverSet1 = new List<RoverData>()
+            {
+                new RoverData(
+                        new Position(2,0,Direction.North),
+                        new RoverInstruction[] { RoverInstruction.Move, RoverInstruction.Move, RoverInstruction .Left, RoverInstruction .Move},
+                        new Position(1,2,Direction.West)
+                    ),
+                new RoverData(
+                        new Position(0,0,Direction.East),
+                        new RoverInstruction[] { RoverInstruction.Left, RoverInstruction.Move, RoverInstruction .Right, RoverInstruction .Move},
+                        new Position(1,1,Direction.East)
+                    )
+            };
+            var boundryPosition = new Position(2, 2);
+
+            var roverSetList = new List<SimulationData>()
+            {
+                new SimulationData(boundryPosition , roverSet1)
+            };
+
+            return roverSetList[entry];
+        }
+
+        [Theory]
+        [InlineData(0)]
+
+        //TODO: Find a better way to check the results of running a simulation
+        public void Simulation_Runs_details_Correctly(int simIndex)
+        {
+            var simulationDetails = GetSimulationDetails(simIndex);
+
+            var testSimulation = new DefaultSimulation(simulationDetails.simulationBoundary);
+
+            foreach (var RoverEntry in simulationDetails.roverList)
+            {
+                testSimulation.addRover(RoverEntry.initialPosition, RoverEntry.instructions);
+            }
+
+            var results = testSimulation.runSimulation();
+
+            var comparedResultList = results.Select((result, index) =>
+            {
+                var expectedPosition = simulationDetails.roverList[index].ExpectedFinalPosition;
+                return (expectedPosition.X == result.X && expectedPosition.Y == result.Y, index);
+            });
+
+            foreach (var (resultValue, index) in comparedResultList)
+            {
+                Assert.True(resultValue, $"There was an issue with Simulation no. {simIndex}. On rover {index}");
+            }
+        }
     }
 }
