4f14e08463522dcb66cf0a8ef4bde80e5c9be827
Aleks <kriegeraleks@protonmail.com>
2021-01-13 00:52:01 +0200

    [test] Add basic tests for simulation
    Test the constructors both work
    test that the simulation adds a rover to the list properly

--- MarsRoverChallengeTestProject/Simulation/Simulation.Tests.cs
+++ MarsRoverChallengeTestProject/Simulation/Simulation.Tests.cs
@@ -1,10 +1,55 @@
-﻿using System;
+using MarsRoverChallenge;
+using MarsRoverChallenge.Rover;
+using MarsRoverChallenge.Simulation;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using Xunit;
 
 namespace MarsRoverChallengeTestProject.Simulation
 {
-    class SimulationTests
+    public class SimulationTests
     {
+        [Fact]
+        public void Simulation_SavesDataCorrectly_WhenPassingPosition()
+        {
+            var positionMock = new Position(0, 0);
+
+            var createdSimulation = new DefaultSimulation(positionMock);
+
+            Assert.Equal(createdSimulation.plateau.boundary, positionMock);
+        }
+
+        [Fact]
+        public void Simulation_SavesDataCorrectly_WhenPassingPoints()
+        {
+            int x = 1;
+            int y = 1;
+
+            var createdSimulation = new DefaultSimulation(x, y);
+
+            Assert.True(x == createdSimulation.plateau.boundary.X && y == createdSimulation.plateau.boundary.Y);
+        }
+
+        [Fact]
+        public void Simulation_AddRover_AddsRoversToList()
+        {
+            var simulationBoundary = new Position(1, 1);
+            var createdSimulation = new DefaultSimulation(simulationBoundary);
+
+            var roverInitialPosition = new Position(0, 0);
+            var roverInstructions = new RoverInstruction[] { RoverInstruction.Left, RoverInstruction.Move };
+
+            createdSimulation.addRover(roverInitialPosition, roverInstructions);
+
+            var (rover, currentPosition, instructionList) = createdSimulation.roverList[0];
+
+            Assert.IsType<DefaultRover>(rover);
+            Assert.Equal(currentPosition, roverInitialPosition);
+            Assert.Equal(instructionList, roverInstructions);
+        }
+
+    }
+}
+
     }
 }
