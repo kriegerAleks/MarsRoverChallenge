9d757b43459d95af5e4964f902eb88d7102d323b
Aleks <kriegeraleks@protonmail.com>
2021-01-12 12:30:52 +0200

    [Test] Add tests for the defaultPlateau
    Add test for position is within bounds, ensure that internal returns yes
    Add for position is outside bonds ensure that it returns no
    Add test ensuring that creation works properly

--- MarsRoverChallengeTestProject/Plateau/DefaultPlateau.Tests.cs
+++ MarsRoverChallengeTestProject/Plateau/DefaultPlateau.Tests.cs
@@ -1,10 +1,61 @@
-﻿using System;
+﻿using MarsRoverChallenge;
+using MarsRoverChallenge.Plateau;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
 
 namespace MarsRoverChallengeTestProject.Plateau
 {
-    class DefaultPlateau
+   public class DefaultPlateauUnitTests
     {
+        
+        [Fact]
+        public void Constructor_GivenBoundryPosition_CanBeRetrieved()
+        {
+            var boundaryPosition = new Position(0,0);
+            var plateau = new DefaultPlateau(boundaryPosition);
+            Assert.Equal(boundaryPosition, plateau.boundary);
+        }
+
+        [Theory]
+        [InlineData(0, 1)]
+        [InlineData(1, 0)]
+        [InlineData(1, 2)]
+        [InlineData(2, 1)]
+        // specifically testing one bounded axis at a time
+        public void positionIsWithinBounds_TrueOnBorders(int x , int y)
+        {
+            var plateau = createPlateau();
+            var TestingPosition = new Position(x, y);
+
+            var isWithinBounds = plateau.positionIsWithinBounds(TestingPosition);
+
+            Assert.True(isWithinBounds);
+        }
+
+        [Theory]
+        [InlineData(-1, 1)]
+        [InlineData(1, -1)]
+        [InlineData(3, 1)]
+        [InlineData(1, 3)]
+        // specifically testing one bounded axis at a time
+        public void positionIsWithinBounds_FalseOnOutsideBorders(int x ,int y)
+        {
+            var plateau = createPlateau();
+            var TestingPosition = new Position(x, y);
+
+            var isWithinBounds = plateau.positionIsWithinBounds(TestingPosition);
+
+            Assert.False(isWithinBounds);
+        }
+
+        private DefaultPlateau createPlateau()
+        {
+            Position BoundryPosition = new Position(2, 2);
+            return new DefaultPlateau(BoundryPosition);
+        }
+
+        
     }
 }
