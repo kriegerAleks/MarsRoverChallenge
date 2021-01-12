61ffd742ffd98683286a64e30c27d4234e70fda0
Aleks <kriegeraleks@protonmail.com>
2021-01-12 14:21:37 +0200

    [test] add tests for direction.test
    Checked convert to direction thows an error on bad conversion
    Checked that the directions return properly when convertingg true
    check that converting from works for all values

--- MarsRoverChallengeTestProject/Rover/Direction.Tests.cs
+++ MarsRoverChallengeTestProject/Rover/Direction.Tests.cs
@@ -1,10 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
+using MarsRoverChallenge.Rover;
 
 namespace MarsRoverChallengeTestProject.Rover
 {
-    class DirectionTests
+    public class DirectionTests
     {
+        [Theory]
+        [InlineData(Direction.North, 'N')]
+        [InlineData(Direction.South, 'S')]
+        [InlineData(Direction.East, 'E')]
+        [InlineData(Direction.West, 'W')]
+        public void convertFromDirection_PrintsProperly(Direction direction , char ExpectedResult)
+        {
+            char TestResult = Directions.convertFromDirection(direction);
+
+            Assert.True(TestResult == ExpectedResult);
+        }
+
+        [Theory]
+        [InlineData(Direction.North, 'N')]
+        [InlineData(Direction.South, 'S')]
+        [InlineData(Direction.East, 'E')]
+        [InlineData(Direction.West, 'W')]
+        public void convertToDirection_ReturnsDirection_WhenValidValue(Direction ExpectedDirection , char toBeConvertedToDirection)
+        {
+            Direction TestDirection = Directions.convertToDirection(toBeConvertedToDirection);
+
+            Assert.True(TestDirection == ExpectedDirection);
+        }
+
+        [Fact]
+        public void convertToDirection_ThrowsError_WhenInvalidValue()
+        {
+            Direction emptyDirection;
+            Exception caughtException = null;
+            try 
+            {
+                emptyDirection = Directions.convertToDirection('X');
+            }
+            catch (Exception cException)
+            {
+                caughtException = cException;
+            }
+
+            Assert.NotNull(caughtException.Message);
+        }
+
     }
 }
